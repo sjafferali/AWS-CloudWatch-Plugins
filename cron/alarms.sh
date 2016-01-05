7e836b7885974e78dbbeb196eb38ef0ff5f349f2
root <root@jump.revmax.com>
2016-01-05 01:18:22 -0600

    Initial commit

---
+++ cron/alarms.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source /opt/aws-scripts-mon/cron/.config
+
+rm -fr /var/tmp/aws-mon
+sleep 90
+if [[ -f /var/tmp/aws-mon/instance-id ]]
+then
+	INSTANCE=$(cat /var/tmp/aws-mon/instance-id)
+else
+	INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+fi
+
+find /opt/aws-scripts-mon/plugins/ -type f -name \*.alarms | while read line
+do
+	plugin=$(echo $line | awk -F/ '{print$5}' | awk -F'.' '{print$1}')
+	if [[ $(eval "echo \$$plugin") -eq 1 ]]
+	then
+		echo $line
+	fi
+done
