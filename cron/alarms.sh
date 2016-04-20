36dd5351e531aa1ce87dcc1d565ba31ed3d3c4a3
root <root@jump.revmax.com>
2016-04-19 23:34:08 -0500

    Added logical order of newly generated hostnames

--- cron/alarms.sh
+++ cron/alarms.sh
@@ -6,11 +6,23 @@ sleep 60
 ec2_host=$(aws ec2 describe-tags --filter "Name=resource-id,Values=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"  "Name=key,Values=Name" | grep Value | awk -F'"' '{print$4}')
 if [[ ! -z $(echo $ec2_host | grep RAND) ]]
 then
-	GEN_NUM=$(echo $[ 1 + $[ RANDOM % 100 ]])
-	ec2_host=$(echo $ec2_host | sed "s/RAND/$GEN_NUM/")
-	aws ec2 create-tags --resources $(curl -s http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name,Value=$ec2_host
+        type_tag=$(aws ec2 describe-tags --filter "Name=resource-id,Values=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"  "Name=key,Values=Type"  | awk -F'"' '/Value/ {print$4}')
+        current_num=$(aws ec2 describe-instances --filter "Name=tag:Type,Values=$type_tag" --output text | awk '/TAGS\s*Name/ {print$3}' | wc -l)
+        while [[ $current_num -gt 0 ]]
+        do
+                if [[ -z $(aws ec2 describe-instances --filter "Name=tag:Type,Values=$type_tag" --output text | awk '/TAGS\s*Name/ {print$3}' | egrep -o [0-9]* | egrep "^$current_num$") ]]
+                then
+                        GEN_NUM=$current_num
+			current_num=$(($current_num-1))
+                else
+                        current_num=$(($current_num-1))
+                fi
+        done
+        ec2_host=$(echo $ec2_host | sed "s/RAND/$GEN_NUM/")
+	echo $ec2_host
+        aws ec2 create-tags --resources $(curl -s http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name,Value=$ec2_host
 fi
-current_host=$(uname -n)
+
 
 if [[ -z $ec2_host ]]
 then
