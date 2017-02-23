b0675e9becea950c3a6b16056bc0c437ffc6c580
Samir Jafferali <samir.jafferali@hulu.com>
2017-02-22 22:37:44 -0800

    Removed AWS specific configuration

--- cron/monitor.sh
+++ cron/monitor.sh
@@ -5,15 +5,7 @@ then
 	exit 1
 fi
 
-
-
 source /opt/aws-scripts-mon/cron/.config
-if [[ -f /var/tmp/aws-mon/instance-id ]]
-then
-	INSTANCE=$(cat /var/tmp/aws-mon/instance-id)
-else
-	INSTANCE=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-fi
 
 
 find /opt/aws-scripts-mon/plugins/ -type f ! -name \*.alarms | while read line
