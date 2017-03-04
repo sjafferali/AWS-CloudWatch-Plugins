24a106f9607316bbd2e6bc1316948a2ff056bd79
Samir Jafferali <samir.jafferali@hulu.com>
2017-03-04 05:30:01 -0800

    Updated interval to use from config file

--- cron/alarms.sh
+++ cron/alarms.sh
@@ -10,7 +10,8 @@ HOST_NAME=$(hostname -s)
 find /opt/aws-scripts-mon/plugins/ -type f -name \*.alarms | while read line
 do
        	plugin=$(echo $line | awk -F/ '{print$5}' | awk -F'.' '{print$1}')
-       	if [[ $(eval "echo \$$plugin") -ge 1 ]]
+        INTERVAL=$(eval "echo \$$plugin")
+       	if [[ $INTERVAL -ge 1 ]]
        	then
        		source $line
        	fi
