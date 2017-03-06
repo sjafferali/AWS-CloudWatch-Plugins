9c2245cffc15795f1a76137d73d95894c077c8e0
Samir Jafferali <samir.jafferali@hulu.com>
2017-03-05 22:32:04 -0800

    Added gitignore to ignore removealarms.sh

--- removealarms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-aws cloudwatch delete-alarms --alarm-names RAM-jump.revmax.com
-aws cloudwatch delete-alarms --alarm-names Disk-jump.revmax.com-/
-aws cloudwatch delete-alarms --alarm-names CPU-jump.revmax.com
-aws cloudwatch delete-alarms --alarm-names Load-jump.revmax.com
-aws cloudwatch delete-alarms --alarm-names IOWait-jump.revmax.com
-aws cloudwatch delete-alarms --alarm-names StatusCheckFailed_Instance-jump.revmax.com
-aws cloudwatch delete-alarms --alarm-names StatusCheckFailed_System-jump.revmax.com
-> /opt/aws-scripts-mon/removealarms.sh
